b4ed3b6f2fc017023a0bfd403179163e0e205f61
qaze <qazedcwsx4@gmail.com>
2021-08-15 14:25:05 +0200

    Wrapped the main function with try-catch to retrieve exception message

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,16 @@
 using namespace VulkanCookbook;
 
 int main() {
-    const std::vector<const char*> extensions = {VK_KHR_SURFACE_EXTENSION_NAME, VK_KHR_WIN32_SURFACE_EXTENSION_NAME};
-    auto library = VulkanLibrary();
-    auto instance = library.getInstance("XD", extensions);
-    auto surface = instance.getSurface();
-    auto device = instance.getLogicalDevice({VK_KHR_SWAPCHAIN_EXTENSION_NAME}, surface);
+    try {
+        const std::vector<const char *> extensions = {VK_KHR_SURFACE_EXTENSION_NAME, VK_KHR_WIN32_SURFACE_EXTENSION_NAME};
+        auto library = VulkanLibrary();
+        auto instance = library.getInstance("XD", extensions);
+        auto surface = instance.getSurface();
+        auto device = instance.getLogicalDevice({VK_KHR_SWAPCHAIN_EXTENSION_NAME}, surface);
+    } catch (std::exception &e) {
+        std::cerr << "encountered exception\n" << e.what();
+        exit(1);
+    }
 
     std::cout << "uwu";
 }
